b0cc020c62af3c03f4edf90abb9928d31d0085d2
Agin-DropDisco <76779267+Agin-DropDisco@users.noreply.github.com>
2021-06-03 14:55:19 +0700

    TT

---
+++ deploy.js
@@ -0,0 +1,139 @@
+const { task } = require("hardhat/config");
+
+// require EXACT bytecodes
+const {
+    abi: factoryAbi,
+    bytecode: factoryBytecode,
+} = require("../dexswap-core/build/DEXswapFactory.json");
+const {
+    abi: feeSetterAbi,
+    bytecode: feeSetterBytecode,
+} = require("../dexswap-core/build/DEXswapFeeSetter.json");
+const {
+    abi: feeReceiverAbi,
+    bytecode: feeReceiverBytecode,
+} = require("../dexswap-core/build/DEXswapFeeReceiver.json");
+const {
+    abi: routerAbi,
+    bytecode: routerBytecode,
+} = require("../dexswap-periphery/build/DEXswapRouter.json");
+
+task(
+    "deploy",
+    "Deploys the whole contracts suite and optionally verifies source code on Etherscan"
+)
+    .addParam(
+        "ownerAddress",
+        "An address that will become the owner of the contracts after deployment."
+    )
+    .addParam(
+        "nativeAssetWrapperAddress",
+        "The address of the contract that wraps the native asset in the target chain"
+    )
+    .setAction(async (taskArguments, hre) => {
+        const { nativeAssetWrapperAddress, ownerAddress } = taskArguments;
+
+        const [accountAddress] = await hre.web3.eth.getAccounts();
+
+        console.log("Using native asset wrapper:", nativeAssetWrapperAddress);
+        console.log("Using account:", accountAddress);
+        console.log(
+            "Deployer's balance:",
+            hre.web3.utils.fromWei(
+                await hre.web3.eth.getBalance(accountAddress)
+            )
+        );
+        console.log();
+
+        console.log("Deploying factory");
+        const factoryContract = await new hre.web3.eth.Contract(factoryAbi)
+            .deploy({
+                data: factoryBytecode.toString("hex"),
+                arguments: [accountAddress], // initially set the fee to setter to the deployer
+            })
+            .send({ from: accountAddress });
+
+        console.log("Deploying fee receiver");
+        const feeReceiverContract = await new hre.web3.eth.Contract(
+            feeReceiverAbi
+        )
+            .deploy({
+                data: feeReceiverBytecode.toString("hex"),
+                arguments: [
+                    ownerAddress,
+                    factoryContract.options.address,
+                    nativeAssetWrapperAddress,
+                    ownerAddress,
+                    ownerAddress,
+                ],
+            })
+            .send({ from: accountAddress });
+
+        console.log("Deploying fee setter");
+        const feeSetterContract = await new hre.web3.eth.Contract(feeSetterAbi)
+            .deploy({
+                data: feeSetterBytecode.toString("hex"),
+                arguments: [ownerAddress, factoryContract.options.address],
+            })
+            .send({ from: accountAddress });
+
+        console.log("Setting correct fee receiver in factory");
+        await factoryContract.methods
+            .setFeeTo(feeReceiverContract.options.address)
+            .send({ from: accountAddress });
+
+        console.log("Setting correct fee setter in factory");
+        await factoryContract.methods
+            .setFeeToSetter(feeSetterContract.options.address)
+            .send({ from: accountAddress });
+
+        // periphery
+        console.log("Deploying router");
+        const routerContract = await new hre.web3.eth.Contract(routerAbi)
+            .deploy({
+                data: routerBytecode.toString("hex"),
+                arguments: [
+                    factoryContract.options.address,
+                    nativeAssetWrapperAddress,
+                ],
+            })
+            .send({ from: accountAddress });
+
+        console.log();
+
+        console.log(`::: Core `);
+
+        console.log(`::: Factory deployed at address ${factoryContract.options.address}`);
+        console.log(`---------------------------------------------------------------------------`);
+
+
+        console.log(`::: Fee setter deployed at address ${feeSetterContract.options.address}`);
+        console.log(`---------------------------------------------------------------------------`);
+
+        console.log(`::: Fee receiver deployed at address ${feeReceiverContract.options.address}`);
+        console.log(`---------------------------------------------------------------------------`);
+        console.log();
+
+        console.log(`::: Periphery`);
+        console.log(`Router deployed at address ${routerContract.options.address}`);
+        console.log(`---------------------------------------------------------------------------`);
+
+        console.log();
+        console.log(`::: Owners `);
+        console.log(`Fee setter owned by address ${await feeSetterContract.methods.owner().call()}`);
+        console.log(`---------------------------------------------------------------------------`);
+
+        console.log(`::: Fee receiver owned by address ${await feeReceiverContract.methods.owner().call()}`);
+        console.log(`---------------------------------------------------------------------------`);
+
+        console.log();
+
+        console.log(`::: Checks`);
+
+        console.log(`Fee setter is set to ${await factoryContract.methods.feeToSetter().call()} in factory`);
+        console.log(`---------------------------------------------------------------------------`);
+
+        console.log(`Fee receiver is set to ${await factoryContract.methods.feeTo().call()} in factory`);
+        console.log(`---------------------------------------------------------------------------`);
+
+    });
---
+++ hardhat.config.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-truffle5");
+require("./tasks/deploy");
+require("./tasks/check-surel");
+
+const INFURA_IDS = process.env.INFURA_ID;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const PK_1 = process.env.PRIVATE_KEY1;
+const PK_2 = process.env.PRIVATE_KEY2;
+const PK_3 = process.env.PRIVATE_KEY3;
+const PK_4 = process.env.PRIVATE_KEY4;
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    
+    networks: {
+        xdai: {
+            url: "https://rpc.xdaichain.com/",
+            accounts: [process.env.PRIVATE_KEY],
+            gasPrice: 1000000000,
+            timeout: 100000,
+        },
+        rinkeby: {
+            url:`https://rinkeby.infura.io/v3/${INFURA_IDS}`,
+            accounts: [process.env.PRIVATE_KEY],
+            network_id: 4,
+            gasPrice: 1000000000,
+            timeout: 100000,
+        },
+        moonbase: {
+            url:"https://rpc.testnet.moonbeam.network",
+            accounts: [PK_1, PK_2, PK_3, PK_4],
+            network_id: 1287,
+            gasPrice: 1000000000,
+            timeout: 100000
+        },
+        oasis: {
+            url:"https://rpc.oasiseth.org:8545",
+            accounts: [process.env.PRIVATE_KEY],
+            network_id: 69,
+            gasPrice: 1000000000,
+            timeout: 100000
+        },
+        matic: {
+            url:"https://rpc-mumbai.matic.today",
+            accounts: [process.env.PRIVATE_KEY],
+            network_id: 80001,
+            gasPrice: 1000000000,
+            timeout: 100000
+        }
+    },
+
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+
+    paths: {
+        sources: "./contracts",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.4",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+            {
+                version: "0.5.16",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+    },
+
+    mocha: {
+        timeout: 20000,
+     },
+};
